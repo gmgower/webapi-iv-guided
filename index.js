78127e7977d4aef34f9c9d095a2be4815df7091f
Mada Gower <gmgower@gmail.com>
2019-11-06 21:43:15 -0600

    Change port.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-// ? s3 install env https://www.npmjs.com/package/dotenv 
+// ? S3 add `"start": "node index.js"` in package.json
+
+// ? s5 check .env is in the getignore file.
+
+// ? s4 install env https://www.npmjs.com/package/dotenv 
 // `yarn add dotenv` and 
 // import and 
 // create folder .env add `PORT=5000`
@@ -7,7 +11,8 @@ require('dotenv').config();
 const server = require('./api/server.js');
 
 // ? s2 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
+// const {PORT} = process.env;
 
 // ? s1 refactor
 server.listen(port, () => {
