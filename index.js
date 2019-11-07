d507276d579c804983b320f4d022f6ef097e4a41
Mada Gower <gmgower@gmail.com>
2019-11-06 19:07:18 -0600

    Initial commit.

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+// ? s3 install env https://www.npmjs.com/package/dotenv 
+// `yarn add dotenv` and 
+// import and 
+// create folder .env add `PORT=5000`
+require('dotenv').config();
+
 const server = require('./api/server.js');
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+// ? s2 
+const port = process.env.PORT;
+
+// ? s1 refactor
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
+
+// server.listen(4000, () => {
+//   console.log('\n*** Server Running on http://localhost:4000 ***\n');
+// });
